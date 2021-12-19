ba567e1d928d863fd06f68e6468a257b82f07ff7
Mauricio Almeron <malmeron@hotmail.com>
2021-12-18 21:27:37 -0300

    Crianddo app Departamento

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'empresas',
-    'funcionarios'
+    'funcionarios',
+    'departamentos',
 ]
 
 MIDDLEWARE = [

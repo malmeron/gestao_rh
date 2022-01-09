93e9bc8e3ed9c4b6bb6647cab2c979382b69eb53
Mauricio Almeron <malmeron@hotmail.com>
2022-01-08 21:26:55 -0300

    static_root novo3

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL = 'home'

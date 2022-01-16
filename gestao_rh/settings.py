3bd8a7918d9a0b6100af6078cd8c7614844a398a
Mauricio Almeron <malmeron@hotmail.com>
2022-01-15 21:31:22 -0300

    Comecando de novo

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -121,33 +121,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"""
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"staticfiles"),
-]
-"""
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,"static"),]
 
-#STATIC_URL = '/static/'
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR,"media")
-#STATIC_ROOT = os.path.join(BASE_DIR,"/static/")
-#ANTIGA
-"""
-STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-"""
+
 STATIC_ROOT = '/home/ubuntu/gestao_rh/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "/static/"),)

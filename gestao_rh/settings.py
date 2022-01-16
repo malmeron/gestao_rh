2fbe711fddede4a84c91c6a253e3c37227358633
Mauricio Almeron <malmeron@hotmail.com>
2022-01-15 21:04:06 -0300

    Alterando parametros staticfiles

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -130,10 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+"""
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"staticfiles"),
 ]
+"""
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,"static"),]
 
 #STATIC_URL = '/static/'
 #MEDIA_URL = '/media/'

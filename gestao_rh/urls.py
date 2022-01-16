1160b6b29b2751fec6922e9490feaf39339ac5b7
Mauricio Almeron <malmeron@hotmail.com>
2022-01-15 21:15:29 -0300

    Alterando parametros urls principal

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('horas-extras/', include('registro_hora_extra.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
